85efd0887e6831541b2c3f04201216e7c4eeef16
Radoslav Mitev <radopz30@gmail.com>
2017-06-01 20:06:37 +0300

    Latest version of my String

--- String Class/String.h
+++ String Class/String.h
@@ -1,5 +1,5 @@
 #pragma once
-
+#include <iostream>
 class String
 {
 private:
@@ -11,10 +11,44 @@ public:
 	String(const String&);
 	int getLength() const { return m_length; };
 	void Append(const String&);
+	char* GetString() const { return m_string; };
 	String Concatenate(const String&) const;
 	String operator+ (const String&) const;
 	String& operator=(const String&);
+	String& operator=(const char*);
 	String& operator+=(const String&);
+	String& operator+=(const char*);
+	String operator+(const char*) const;
+	String SubString(int, int) const;
+	int FromStringToInt() const;
+	double FromStringToDouble() const;
+	void Clear();
+	bool operator==(const String&) const;
+	bool operator==(const char*) const;
+	bool operator<(const char*) const;
+	bool operator<(const String&) const;
+	bool operator>(const char*) const;
+	bool operator>(const String&) const;
+	bool operator>=(const String&) const;
+	bool operator>=(const char*) const;
+	bool operator<=(const char*) const;
+	bool operator<=(const String&) const;
+	char& operator[](int) const;
+	bool isThereAChar(char) const;
+	void RemoveAllIntervalsInString();
+	void PushBack(char);
+	void RemoveChar(int);
 	~String();
 	void Print();
 };
+
+void StrCopy(char*, const char*);
+int StrLength(const char*);
+int StrCompr(const char*, const char*);
+std::ostream& operator<< (std::ostream&, const String&);
+std::istream& operator>> (std::istream&, String&);
+std::istream& getline(std::istream&, String&);
+std::istream& getWord(std::istream&, String&);
+String FromNumberToString(double);
+template <typename T>
+T PowFunction(T a, int n);
